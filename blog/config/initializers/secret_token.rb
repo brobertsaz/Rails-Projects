c0d999ee6718fee70770ad916c3050e652c5078e
Bob Roberts <bob@ubuntu2.(none)>
2011-02-27 19:44:16 -0700

    initial commit

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '580d7f0c2375d72aec6f75f595eaf2c8c5b33c21939f6e7b6e7b197640c28cd5374024b56a507bcc9a6de10100b03c029c9f3c0b14e15f31147cfbed37941aa7'
---
+++ depot2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot2::Application.config.secret_token = 'd4a37f7dd4158de57b0994e0891da2fb402895c6a07d5989b90960995359f367df1f8723a28651ce4a1cf7f8f34f891da777ed2d749474ae7fc6b4706b10d51c'
